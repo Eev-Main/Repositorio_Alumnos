9951ee9ce2e0b5b71b5cd8fadd6d84ad08759717
victor Gonzalez <victorantonio6@gmail.com>
2021-03-04 13:45:39 -0700

    introduccion a encapsulamiento

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Repositorio_Alumnos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creacion del alumno/persona
+            Persona alumno1 = new Persona("Jose", "perez");
+            
+            //realizar accion
+            Console.WriteLine(alumno1.NombreCompleto);
+            alumno1.presentarse();
+            
+            //cambiar apellido
+            alumno1.apellido = "Mendez";
+            Console.WriteLine(alumno1.NombreCompleto);
+
+            //color favorito
+            alumno1.ColorFavorito = "Rojo";
+            Console.WriteLine(alumno1.ColorFavorito);
+        }
+    }
+}
---
+++ persona.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace Repositorio_Alumnos
+{
+    class Persona
+    {
+        //caracteristicas de la persona
+        public string nombre;
+        public string apellido;
+
+        //Propetyy
+        // - public
+        // - en C#, comienza con mayuscula
+        // - tiene get y/o set
+        public string NombreCompleto
+        {
+            get => this.nombre + " " + this.apellido;
+        }
+
+        private string colorFavorito;
+        
+        public string ColorFavorito
+        {
+            get => "color " + this.colorFavorito;
+            //valce esta disponible dentro del set
+            //quiere decir el valor que se va 
+            //a asignar
+            set => this.colorFavorito = value;
+        }
+
+        //definiendo a la persona
+        public Persona(string nombre, string apellido)
+        {
+         this.nombre = nombre;
+         this.apellido = apellido;
+        }
+        
+        //accion
+        public void presentarse()
+        {
+            Console.WriteLine("Mucho gusto, me llamo " + this.nombre + " " + this.apellido);
+            //Console.WriteLine($"Hola, mi nombre es {this.nombre} {this.apellido}");
+            //Console.WriteLine($"Hola, mi nombre es {0} {1}", this.nombre, this apellido);
+        }
+    }
+}
